d1190a4c980e8adf88f612a308480572d4aa72d0
Guilherme Brasolin <guilherme.castello@outlook.com>
2022-12-15 16:24:42 -0300

    turning strictmode off

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,8 @@ import "../src/styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    {/* <React.StrictMode> */}
+    <App />
+    {/* </React.StrictMode> */}
   </BrowserRouter>
 );
