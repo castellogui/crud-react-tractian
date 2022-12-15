cfcba1e3d4d64a033c8f884f21cec2de4a96bd4e
Guilherme Brasolin <guilherme.castello@outlook.com>
2022-12-15 13:32:43 -0300

    add list of users in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import Frame from "../components/Frame";
+import SearchableListUsers from "../components/SearchableListUsers";
 
 export default function Home() {
   const [visible, setVisible] = useState(false);
@@ -10,19 +11,19 @@ export default function Home() {
       <Navbar openSidebarFunction={setVisible}></Navbar>
       <Sidebar visible={visible} callbackClose={setVisible}></Sidebar>
       <div className="flex flex-row justify-center">
-        <Frame height="half-screen" width={30}>
+        <Frame height="half-screen" width={46}>
           <span className="title">Units</span>
         </Frame>
-        <Frame height="half-screen" width={30}>
-          <span className="title">Users</span>
-        </Frame>
-        <Frame height="half-screen" width={30}>
-          <span className="title">Assets</span>
+        <Frame height="half-screen" width={46}>
+          <>
+            <span className="title">Users</span>
+            <SearchableListUsers></SearchableListUsers>
+          </>
         </Frame>
       </div>
       <div className="flex flex-row justify-center">
         <Frame height="half-screen" width={95}>
-          <span className="title">News</span>
+          <span className="title">Assets</span>
         </Frame>
       </div>
     </>
