f4eb35fea124205d6bfbc4eecb5e23fcbd6eb3d3
Guilherme Brasolin <guilherme.brasolin@g-ushin.com>
2022-12-15 09:52:38 -0300

    adding components in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,30 @@
-import { Drawer } from "antd";
+import { useState } from "react";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import Frame from "../components/Frame";
 
 export default function Home() {
+  const [visible, setVisible] = useState(false);
   return (
     <>
-      1<Drawer open={true}>This is the content of Drawer</Drawer>
+      <Navbar openSidebarFunction={setVisible}></Navbar>
+      <Sidebar visible={visible} callbackClose={setVisible}></Sidebar>
+      <div className="flex flex-row justify-center">
+        <Frame height="half-screen" width={30}>
+          <span className="title">Units</span>
+        </Frame>
+        <Frame height="half-screen" width={30}>
+          <span className="title">Users</span>
+        </Frame>
+        <Frame height="half-screen" width={30}>
+          <span className="title">Assets</span>
+        </Frame>
+      </div>
+      <div className="flex flex-row justify-center">
+        <Frame height="half-screen" width={95}>
+          <span className="title">News</span>
+        </Frame>
+      </div>
     </>
   );
 }
