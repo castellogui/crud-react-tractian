d6873c1ab78b506b2b973f52625a3678aca978f8
Guilherme Brasolin <guilherme.castello@outlook.com>
2022-12-15 11:01:05 -0300

    adding overflow hidden and box-sizing on frame

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -20,7 +20,7 @@ export default function Frame(props: FrameProps) {
         height: sizesH[props.height],
         width: `${props.width}%`,
       }}
-      className="relative my-4 mx-4 rounded-lg max-h-[calc(100vh-8rem)] inline-block py-6 px-4"
+      className="relative my-4 mx-4 rounded-lg max-h-[calc(100vh-8rem)] inline-block py-6 px-4 overflow-hidden box-border"
     >
       <div className="w-full h-5 bg-[#245ce4] rounded-t-[0.5rem] absolute top-0 left-0"></div>
       {props.children}
