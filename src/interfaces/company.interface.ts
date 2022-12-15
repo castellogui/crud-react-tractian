1359493ec4938d374ae7e697edd139703173924f
Guilherme Brasolin <guilherme.castello@outlook.com>
2022-12-15 16:23:47 -0300

    deleting wrong interfaces

--- src/interfaces/company.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Unit } from "./unit.interface";
-import { User } from "./user.interface";
-
-export interface Company {
-  name: String;
-  units: Array<Unit>;
-  users: Array<User>;
-  created_at: Date;
-}
