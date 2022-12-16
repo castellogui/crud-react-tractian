2e1f43f799329677520fe6c3e0f22d2b88e077f8
Guilherme Brasolin <guilherme.castello@outlook.com>
2022-12-16 10:15:09 -0300

    creating userlogged reducer and adding to the store

---
+++ src/store/reducers/userLogged.ts
@@ -0,0 +1,59 @@
+import { logUser } from "../../interfaces/actions/actions.interface";
+import { User } from "../../interfaces/models/user.interface";
+import { LOG_USER } from "../actions/actionTypes";
+
+const initialState: User = {
+  _id: "",
+  name: "",
+  familyName: "",
+  username: "",
+  email: "",
+  password: "",
+  avatar: "",
+  created_at: "",
+  lastLogin: "",
+  company: {
+    _id: "",
+    name: "",
+    units: [],
+    users: [],
+    created_at: "",
+    __v: 0,
+  },
+  type: [],
+  __v: 0,
+};
+
+export const userLoggedReducer = (state = initialState, action: logUser) => {
+  switch (action.type) {
+    case LOG_USER:
+      if (action.payload != undefined) {
+        return {
+          ...state,
+          _id: action.payload._id,
+          name: action.payload.name,
+          familyName: action.payload.familyName,
+          username: action.payload.username,
+          email: action.payload.email,
+          password: action.payload.password,
+          avatar: action.payload.avatar,
+          created_at: action.payload.created_at,
+          lastLogin: action.payload.lastLogin,
+          company: {
+            _id: action.payload._id,
+            name: action.payload.company.name,
+            units: action.payload.company.units,
+            users: action.payload.company.users,
+            created_at: action.payload.company.created_at,
+            __v: action.payload.company.__v,
+          },
+          type: action.payload.type,
+          __v: action.payload.__v,
+          token: action.payload.token,
+        };
+      }
+      return initialState;
+    default:
+      return state;
+  }
+};
--- src/store/store.config.ts
+++ src/store/store.config.ts
@@ -1,8 +1,10 @@
 import { createStore, combineReducers } from "redux";
 import { unitReducer } from "./reducers/unitReducer";
+import { userLoggedReducer } from "./reducers/userLogged";
 
 const reducers = combineReducers({
   unitState: unitReducer,
+  userLogged: userLoggedReducer,
 });
 
 function storeConfig() {
