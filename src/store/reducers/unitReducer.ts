50f5ee7ede65adb61f5f89dbbcae00155c1dbdf4
Guilherme Brasolin <guilherme.castello@outlook.com>
2022-12-15 23:07:49 -0300

    adding verification to undefined units sended to reducer

--- src/store/reducers/unitReducer.ts
+++ src/store/reducers/unitReducer.ts
@@ -21,23 +21,26 @@ const initialState = {
 export const unitReducer = (state = initialState, action: changeUnit) => {
   switch (action.type) {
     case CHANGE_UNIT:
-      return {
-        ...state,
-        _id: action.payload._id,
-        name: action.payload.name,
-        zipCode: action.payload.zipCode,
-        created_at: action.payload.created_at,
-        assets: action.payload.assets,
-        company: {
-          _id: action.payload.company._id,
-          name: action.payload.company.name,
-          units: action.payload.company.units,
-          users: action.payload.company.users,
-          created_at: action.payload.company.created_at,
-          __v: action.payload.company.__v,
-        },
-        __v: action.payload.__v,
-      };
+      if (action.payload != undefined) {
+        return {
+          ...state,
+          _id: action.payload._id,
+          name: action.payload.name,
+          zipCode: action.payload.zipCode,
+          created_at: action.payload.created_at,
+          assets: action.payload.assets,
+          company: {
+            _id: action.payload.company._id,
+            name: action.payload.company.name,
+            units: action.payload.company.units,
+            users: action.payload.company.users,
+            created_at: action.payload.company.created_at,
+            __v: action.payload.company.__v,
+          },
+          __v: action.payload.__v,
+        };
+      }
+      return initialState;
     default:
       return state;
   }
