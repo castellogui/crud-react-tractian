6b142c6470585d7e7795b398f571451ac8023fa1
Guilherme Brasolin <guilherme.castello@outlook.com>
2022-12-16 14:23:17 -0300

    adding query client in root

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,22 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "../src/styles/index.css";
 import { Provider } from "react-redux";
+import { QueryClientProvider, QueryClient } from "react-query";
 import configStore from "../src/store/store.config";
 
 const store = configStore;
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
-    <BrowserRouter>
-      {/* <React.StrictMode> */}
-      <App />
-      {/* </React.StrictMode> */}
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        {/* <React.StrictMode> */}
+        <App />
+        {/* </React.StrictMode> */}
+      </BrowserRouter>
+    </QueryClientProvider>
   </Provider>
 );
