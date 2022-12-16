91049a6c8c5d0f3c60d49d9bed31a64f1c70d60b
Guilherme Brasolin <guilherme.castello@outlook.com>
2022-12-16 10:14:02 -0300

    adding userLoggedState on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,6 +8,7 @@ import { Unit } from "../interfaces/models/unit.interface";
 import units from "../assets/units.json";
 
 interface NavbarProps {
+  userLogged: any;
   changeUnit: any;
   unit: any;
   openSidebarFunction: Function;
@@ -40,45 +41,54 @@ function Navbar(props: NavbarProps) {
     onClick: handleMenuClick,
   };
 
-  return (
-    <nav className="w-[100%] h-16 bg-white shadow-md flex flex-row items-center justify-between">
-      <MenuOutlined
-        className="mx-4 scale-125"
-        onClick={() => {
-          props.openSidebarFunction(true);
-        }}
-      ></MenuOutlined>
-      <div className="w-max h-12 flex flex-row text-center items-center ">
-        <div className="mx-8">
-          <Dropdown menu={menuProps}>
-            <Button>
-              <Space>
-                Units
-                <DownOutlined />
-              </Space>
-            </Button>
-          </Dropdown>
-        </div>
-        <div className="w-max h-full flex flex-col">
-          <div className="w-full h-full">
-            <span className="w-full h-full text-left font-medium">Unidade Mooca</span>
+  function renderNav() {
+    return (
+      <nav className="w-[100%] h-16 bg-white shadow-md flex flex-row items-center justify-between">
+        <MenuOutlined
+          className="mx-4 scale-125"
+          onClick={() => {
+            props.openSidebarFunction(true);
+          }}
+        ></MenuOutlined>
+        <div className="w-max h-12 flex flex-row text-center items-center ">
+          <div className="mx-8">
+            <Dropdown menu={menuProps}>
+              <Button>
+                <Space>
+                  Units
+                  <DownOutlined />
+                </Space>
+              </Button>
+            </Dropdown>
           </div>
-          <div className="w-full h-full flex flex-row gap-3">
-            <span className="w-full h-auto text-[#404040]">Account</span>
-            <span className="w-full h-auto text-[#404040]">Log off</span>
+          <div className="w-max h-full flex flex-col">
+            <div className="w-full h-full">
+              <span className="w-full h-full text-left font-medium">
+                Unit {props.unit.name == "" ? "All" : props.unit.name}
+              </span>
+            </div>
+            <div className="w-full h-full flex flex-row gap-3">
+              <span className="w-full h-auto text-[#404040]">Account</span>
+              <span className="w-full h-auto text-[#404040]">Log off</span>
+            </div>
           </div>
+          <img
+            src="https://cdn.lorem.space/images/face/.cache/200x200/charles-etoroma-95UF6LXe-Lo-unsplash.jpg"
+            alt="logged-profile-avatar"
+            className="w-auto h-full rounded-full mx-4 "
+          />
         </div>
-        <img
-          src="https://cdn.lorem.space/images/face/.cache/200x200/charles-etoroma-95UF6LXe-Lo-unsplash.jpg"
-          alt="logged-profile-avatar"
-          className="w-auto h-full rounded-full mx-4 "
-        />
-      </div>
-    </nav>
-  );
+      </nav>
+    );
+  }
+
+  return <>{props.userLogged._id == "" ? renderNav() : null}</>;
 }
 
-const mapStateToProps = (state: any) => ({ unit: state.unitState });
+const mapStateToProps = (state: any) => ({
+  unit: state.unitState,
+  userLogged: state.userLogged,
+});
 const mapDispatchToProps = (dispatch: any) => {
   return {
     changeUnit: (unit: Unit) => dispatch(changeUnit(unit)),
