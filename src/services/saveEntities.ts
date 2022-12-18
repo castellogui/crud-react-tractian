d40ec9fdd169b2e2d4c1d9c9785670f09d58c5a9
Guilherme Brasolin <guilherme.castello@outlook.com>
2022-12-18 17:35:08 -0300

    adding edit company in save entities file before refactoring

--- src/services/saveEntities.ts
+++ src/services/saveEntities.ts
@@ -1,7 +1,21 @@
 import baseAxios from "../axios/config";
+import { UpdatedCompany } from "../interfaces/models/company.interface";
 
 export async function editUnitData(token: String | undefined, data: any, id: string | undefined) {
   let response = await baseAxios.put(`/units/edit/${id}`, data, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return await response.data;
+}
+
+export async function editCompanyData(
+  token: String | undefined,
+  data: UpdatedCompany,
+  id: string | undefined
+) {
+  let response = await baseAxios.put(`/companies/edit/${id}`, data, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
