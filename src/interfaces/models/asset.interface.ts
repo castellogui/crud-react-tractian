609778dc1879ce0e94cfa10f8eed4dcdead0605d
Guilherme Brasolin <guilherme.castello@outlook.com>
2022-12-19 15:46:25 -0300

    creating updated type

--- src/interfaces/models/asset.interface.ts
+++ src/interfaces/models/asset.interface.ts
@@ -32,3 +32,38 @@ export interface Asset {
   created_at: string;
   __v: number;
 }
+
+export interface UpdatedAsset {
+  _id?: string;
+  name?: string;
+  unit?: {
+    _id?: string;
+    name?: string;
+    zipCode?: string;
+    created_at?: string;
+    assets?: string[];
+    company?: string;
+    __v?: number;
+  };
+  description?: string;
+  avatar?: string;
+  model?: string;
+  owner?: {
+    _id?: string;
+    name?: string;
+    familyName?: string;
+    username?: string;
+    email?: string;
+    password?: string;
+    avatar?: string;
+    created_at?: string;
+    lastLogin?: string;
+    company?: string;
+    type?: string[];
+    __v?: number;
+  };
+  status?: string;
+  healthLevel?: string;
+  created_at?: string;
+  __v?: number;
+}
