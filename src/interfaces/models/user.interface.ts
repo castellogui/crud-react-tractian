85669227d9c77cfb9627ac723ea5dc78a9c8065f
Guilherme Brasolin <guilherme.castello@outlook.com>
2022-12-19 17:57:58 -0300

    adding updated user interface to user

--- src/interfaces/models/user.interface.ts
+++ src/interfaces/models/user.interface.ts
@@ -1,6 +1,3 @@
-import { Company } from "./company.interface";
-import { Unit } from "./unit.interface";
-
 export interface User {
   _id: string;
   name: string;
@@ -21,5 +18,28 @@ export interface User {
   };
   type: string[];
   __v: number;
+  token?: string;
+}
+
+export interface UpdatedUser {
+  _id?: string;
+  name?: string;
+  familyName?: string;
+  username?: string;
+  email?: string;
+  password?: string;
+  avatar?: string;
+  created_at?: string;
+  lastLogin?: string;
+  company?: {
+    _id?: string;
+    name?: string;
+    units?: string[];
+    users?: string[];
+    created_at?: string;
+    __v?: number;
+  };
+  type?: string[];
+  __v?: number;
   token?: string;
 }
