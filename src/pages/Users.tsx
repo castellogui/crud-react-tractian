82b1b65b3089260f4cd878780353c2951d3a46f5
Guilherme Brasolin <guilherme.castello@outlook.com>
2022-12-19 17:57:26 -0300

    creating edit function to users

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -3,13 +3,17 @@ import { connect } from "react-redux";
 import SearchableListUsers from "../components/SearchableListUsers";
 import { Unit } from "../interfaces/models/unit.interface";
 import { useState } from "react";
-import { User } from "../interfaces/models/user.interface";
+import { UpdatedUser, User } from "../interfaces/models/user.interface";
 import { DownOutlined } from "@ant-design/icons";
 import { Avatar, Input, Dropdown, Button, Space, MenuProps } from "antd";
 import companies from "../assets/companies.json";
+import { handleChange, handleChangeInputElement } from "../utils/HandlesUtils";
+import { confirmMessage, showMessage } from "../utils/MessageUtils";
+import { editUserData } from "../services/saveEntities";
 
 interface UsersProps {
   unit: Unit;
+  userLogged: User;
 }
 
 function Users(props: UsersProps) {
@@ -25,8 +29,16 @@ function Users(props: UsersProps) {
     items.push(unitOption);
   });
 
+  const filterAdminTypeWord = () => {
+    let userType: any = menuProps.items.filter((option) => {
+      return option.key == user?.type[0];
+    });
+    return userType[0]?.label;
+  };
+
   const handleMenuClick: MenuProps["onClick"] = (e) => {
-    let type = e.key;
+    let type = [e.key];
+    handleChange("type", type, user, setUser);
   };
 
   const menuProps = {
@@ -60,32 +72,58 @@ function Users(props: UsersProps) {
           </div>
           <div className="px-5">
             <Input
-              defaultValue={user?.name}
+              name="name"
+              value={user?.name}
+              onChange={(e) => {
+                handleChangeInputElement(e, user, setUser);
+              }}
               placeholder="Name"
               style={{ width: "30%", margin: "0.5rem" }}
             ></Input>
             <Input
-              defaultValue={user?.familyName}
+              name="familyName"
+              value={user?.familyName}
+              onChange={(e) => {
+                handleChangeInputElement(e, user, setUser);
+              }}
               placeholder="Family Name"
               style={{ width: "30%", margin: "0.5rem" }}
             ></Input>
             <Input
-              defaultValue={user?.username}
+              name="username"
+              value={user?.username}
+              onChange={(e) => {
+                handleChangeInputElement(e, user, setUser);
+              }}
               placeholder="Username"
               style={{ width: "30%", margin: "0.5rem" }}
             ></Input>
             <Input
-              defaultValue={user?.email}
+              name="email"
+              value={user?.email}
+              onChange={(e) => {
+                handleChangeInputElement(e, user, setUser);
+              }}
               placeholder="Email"
               style={{ width: "30%", margin: "0.5rem" }}
             ></Input>
             <Input
+              name="password"
               defaultValue={"password"}
+              onChange={(e) => {
+                handleChangeInputElement(e, user, setUser);
+              }}
               type="password"
               placeholder="Password"
               style={{ width: "30%", margin: "0.5rem" }}
             ></Input>
-            <Dropdown menu={menuProps} className="m-2 w-[30%]">
+            <Input
+              name="type"
+              value={filterAdminTypeWord()}
+              readOnly
+              style={{ width: "30%", margin: "0.5rem", cursor: "not-allowed" }}
+            ></Input>
+            <Dropdown menu={menuProps} className="m-2 w-[15%]" trigger={["click"]}>
               <Button>
                 <Space>
                   Type
@@ -97,6 +135,43 @@ function Users(props: UsersProps) {
         </div>
       </>
     );
+  }
+
+  async function updateUserInfo() {
+    await confirmMessage(
+      undefined,
+      "Are you sure?",
+      "Do you want to save the changes?",
+      undefined,
+      "Save",
+      "Cancel"
+    ).then(async (result: any) => {
+      if (result.isConfirmed) {
+        try {
+          let data: UpdatedUser = {
+            name: user?.name,
+            familyName: user?.familyName,
+            username: user?.username,
+            email: user?.email,
+            password: user?.password,
+            type: user?.type,
+          };
+          await editUserData(props.userLogged.token, data, user?._id);
+          showMessage("success", undefined, "Changes were updated", "");
+        } catch (error: any) {
+          showMessage(
+            "error",
+            undefined,
+            "There is an error while trying to update value.",
+            `Error detail: ${error.response.data.errorDetail}`
+          );
+        }
+      } else {
+        if (result.isDenied) {
+          showMessage("info", undefined, "Changes was not updated", "");
+        }
+      }
+    });
   }
 
   return (
@@ -118,13 +193,14 @@ function Users(props: UsersProps) {
                 <div className="w-full h-full relative flex flex-col justify-center">
                   {user == undefined ? renderSelectUser() : renderEditInputs()}
                 </div>
-                {/* <Button
+                <Button
                   style={{ visibility: user != undefined ? "visible" : "hidden" }}
                   className="absolute bottom-0
                    right-4 bg-[#20bd5a] text-white font-bold"
+                  onClick={updateUserInfo}
                 >
                   Save
-                </Button> */}
+                </Button>
                 <Button
                   style={{ visibility: user != undefined ? "visible" : "hidden" }}
                   className="absolute bottom-0 left-4 font-bold"
@@ -145,6 +221,7 @@ function Users(props: UsersProps) {
 
 const MapStateToProps = (state: any) => ({
   unit: state.unitState,
+  userLogged: state.userLogged,
 });
 
 export default connect(MapStateToProps)(Users);
