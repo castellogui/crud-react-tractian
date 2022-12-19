24d6b14d328bb8c14af747a75da9f367bf8cd829
Guilherme Brasolin <guilherme.castello@outlook.com>
2022-12-19 13:03:07 -0300

    creating handleChange for other elements

--- src/utils/HandlesUtils.ts
+++ src/utils/HandlesUtils.ts
@@ -1,8 +1,12 @@
-export default function handleChange(
+export function handleChangeInputElement(
   event: React.FormEvent<HTMLInputElement>,
   state: any,
   setState: any
 ) {
   let target = event.target as HTMLInputElement;
   setState({ ...state, [target.name]: target.value });
+}
+
+export function handleChange(key: string, value: string, state: any, setState: any) {
+  setState({ ...state, [key]: value });
 }
